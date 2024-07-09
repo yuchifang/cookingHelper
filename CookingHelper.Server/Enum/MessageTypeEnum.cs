f1c00918a79e196d1063fa4dbfbc1dca8e43d638
fangfelipe <fangfelipe@gmail.com>
2024-07-09 17:35:34 +0800

    add FlexMessage

--- CookingHelper.Server/Enum/MessageTypeEnum.cs
+++ CookingHelper.Server/Enum/MessageTypeEnum.cs
@@ -4,10 +4,33 @@ public static class MessageTypeEnum
 {
     public const string Text = "text";
     public const string Postback = "postback";
+
+    public const string Flex = "flex";
 }
 
 public static class ActionTypeEnum
 {
     public const string Message = "message";
     public const string Postback = "postback";
+}
+
+public static class FlexContainerTypeEnum
+{
+    public const string Bubble = "bubble";
+    public const string Carousel = "carousel";
+}
+
+public static class FlexComponentTypeEnum
+{
+    public const string Box = "box";
+    public const string Text = "text";
+
+    public const string Separator = "separator";
+    public const string Button = "button";
+}
+
+public static class FlexComponentLayoutTypeEnum
+{
+    public const string Vertical = "vertical";
+    public const string Horizontal = "horizontal";
 }
---
+++ CookingHelper.Server/LineDto/MessageGroup/FlexMessageObject.cs
@@ -0,0 +1,122 @@
+using CookingHelper.Enum;
+
+namespace CookingHelper.LineDto;
+
+public class FlexMessageObject<T> : BaseMessageObject
+{
+    public FlexMessageObject()
+    {
+        Type = MessageTypeEnum.Flex;
+    }
+
+    public string AltText { get; set; } = default!;
+
+    public T Contents { get; set; } = default!;
+}
+
+public class FlexBubbleContainer
+{
+    public string Type { get; set; } = FlexContainerTypeEnum.Bubble;
+    public string? Size { get; set; }
+    public string? Direction { get; set; }
+
+    public FlexComponent? Header { get; set; }
+    public FlexComponent? Hero { get; set; }
+    public FlexComponent? Body { get; set; }
+    public FlexComponent? Footer { get; set; }
+    public FlexBubbleContainerStyle? Styles { get; set; }
+    public ActionDto? Action { get; set; }
+}
+
+public class FlexCarouselContainer
+{
+    public string Type { get; set; } = FlexContainerTypeEnum.Carousel;
+    public List<FlexBubbleContainer> Contents { get; set; }
+}
+
+// component
+public class FlexComponent
+{
+    public string Type { get; set; }
+
+    // box component
+    public string? Layout { get; set; }
+    public List<FlexComponent>? Contents { get; set; }
+    public string? BackgroundColor { get; set; }
+    public string? BorderColor { get; set; }
+    public string? BorderWidth { get; set; }
+    public string? CornerRadius { get; set; }
+    public string? Width { get; set; }
+    public string? MaxWidth { get; set; }
+    public string? Height { get; set; }
+    public string? MaxHeight { get; set; }
+    public int? Flex { get; set; }
+    public string? Spacing { get; set; }
+    public string? Mergin { get; set; }
+    public string? PaddingAll { get; set; }
+    public string? PaddingTop { get; set; }
+    public string? PaddingBottom { get; set; }
+    public string? PaddingStart { get; set; }
+    public string? PaddingEnd { get; set; }
+    public string? Position { get; set; }
+    public string? OffsetTop { get; set; }
+    public string? OffsetBottom { get; set; }
+    public string? OffsetStart { get; set; }
+    public string? OffsetEnd { get; set; }
+    public ActionDto? Action { get; set; }
+    public string? JustifyContent { get; set; }
+    public string? AlignItems { get; set; }
+    public FlexBackgroundDto? Background { get; set; }
+
+    // button coponent
+    public string? Style { get; set; }
+    public string? Color { get; set; }
+    public string? Gravity { get; set; }
+    public string? AdjustMode { get; set; }
+
+    //image component
+    public string? Url { get; set; }
+    public string? Align { get; set; }
+    public string? Size { get; set; }
+    public string? AspectRatio { get; set; }
+    public string? AspectMode { get; set; }
+    public bool? Animated { get; set; }
+
+    //text component
+    public string? Text { get; set; }
+    public bool? Wrap { get; set; }
+    public string? LineSpaceing { get; set; }
+    public int? Maxlines { get; set; }
+    public string? Weight { get; set; }
+    public string? Decoration { get; set; }
+
+    public string? Margin { get; set; }
+
+    //icon, span, separator, filler 這四個元件所需屬性已在上面宣告過，故不用重複宣告
+}
+
+public class FlexBackgroundDto
+{
+    public string? Type { get; set; }
+    public string? Angle { get; set; }
+    public string? StartColor { get; set; }
+    public string? EndColor { get; set; }
+    public string? CenterColor { get; set; }
+    public string? CenterPosition { get; set; }
+}
+
+// bubble container styles
+public class FlexBubbleContainerStyle
+{
+    public FlexBlockStyle? Header { get; set; }
+    public FlexBlockStyle? Hero { get; set; }
+    public FlexBlockStyle? Body { get; set; }
+    public FlexBlockStyle? Footer { get; set; }
+}
+
+public class FlexBlockStyle
+{
+    public string? BackgroundColor { get; set; }
+    public bool? Separator { get; set; }
+    public string? SeparatorColor { get; set; }
+}
