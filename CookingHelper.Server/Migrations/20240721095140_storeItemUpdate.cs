8ff173a1ee94296d7bf49207b55e7856bae57f9d
fangfelipe <fangfelipe@gmail.com>
2024-07-23 19:17:12 +0800

    add migration

---
+++ CookingHelper.Server/Migrations/20240721095140_storeItemUpdate.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CookingHelper.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class storeItemUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_StoreItem_StoreList_StoreItemGroupId",
+                table: "StoreItem");
+
+            migrationBuilder.RenameColumn(
+                name: "StoreItemGroupId",
+                table: "StoreItem",
+                newName: "StoreItemId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StoreItem_StoreList_StoreItemId",
+                table: "StoreItem",
+                column: "StoreItemId",
+                principalTable: "StoreList",
+                principalColumn: "StoreListId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_StoreItem_StoreList_StoreItemId",
+                table: "StoreItem");
+
+            migrationBuilder.RenameColumn(
+                name: "StoreItemId",
+                table: "StoreItem",
+                newName: "StoreItemGroupId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StoreItem_StoreList_StoreItemGroupId",
+                table: "StoreItem",
+                column: "StoreItemGroupId",
+                principalTable: "StoreList",
+                principalColumn: "StoreListId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ CookingHelper.Server/Migrations/20240721102527_storeItemIDUpdate.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CookingHelper.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class storeItemIDUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_StoreItem_StoreList_StoreItemId",
+                table: "StoreItem");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "StoreItemId",
+                table: "StoreItem",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StoreItem_StoreListId",
+                table: "StoreItem",
+                column: "StoreListId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StoreItem_StoreList_StoreListId",
+                table: "StoreItem",
+                column: "StoreListId",
+                principalTable: "StoreList",
+                principalColumn: "StoreListId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_StoreItem_StoreList_StoreListId",
+                table: "StoreItem");
+
+            migrationBuilder.DropIndex(
+                name: "IX_StoreItem_StoreListId",
+                table: "StoreItem");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "StoreItemId",
+                table: "StoreItem",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StoreItem_StoreList_StoreItemId",
+                table: "StoreItem",
+                column: "StoreItemId",
+                principalTable: "StoreList",
+                principalColumn: "StoreListId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
