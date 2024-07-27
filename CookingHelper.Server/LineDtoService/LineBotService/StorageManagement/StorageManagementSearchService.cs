cf9d11bb557c8b54fe1e7b020e96b1d0e983f276
fangfelipe <fangfelipe@gmail.com>
2024-07-27 12:21:31 +0800

    add StorageSearchUI

--- CookingHelper.Server/LineDtoService/LineBotService/StorageManagement/StorageManagementSearchService.cs
+++ CookingHelper.Server/LineDtoService/LineBotService/StorageManagement/StorageManagementSearchService.cs
@@ -1,3 +1,4 @@
+using CookingHelper.DatabaseService;
 using CookingHelper.Enum;
 using CookingHelper.LineDto;
 using CookingHelper.LineDtoService;
@@ -7,12 +8,17 @@ using static CookingHelper.Utils;
 public class StorageManagementSearchService
 {
     private readonly StorageManagementService _storageManagementService;
+    private readonly StorageManagementDatabaseService _storageManagementDatabaseService;
 
     private static List<object> _ReplyMessageListStatic = new List<object>();
 
-    public StorageManagementSearchService(StorageManagementService StorageManagementService)
+    public StorageManagementSearchService(
+        StorageManagementService StorageManagementService,
+        StorageManagementDatabaseService StorageManagementDatabaseService
+    )
     {
         _storageManagementService = StorageManagementService;
+        _storageManagementDatabaseService = StorageManagementDatabaseService;
     }
 
     public async Task SearchStorage(WebhookEventDto WebHookEventDto)
@@ -27,18 +33,148 @@ public class StorageManagementSearchService
         {
             StringToStorageInfo(
                 WebHookEventMessage,
-                out Dictionary<string, string> StorageInfoDic,
+                out StorageInfo StorageInfo,
                 out string ErrorText
             );
             if (ErrorText != "")
             {
-                _ReplyMessageListStatic = new List<object>
-                {
-                    new TextMessageObject { Text = "發生錯誤: 此欄位出現問題 " + ErrorText },
-                };
+                await _storageManagementService.GetStorage(WebHookEventDto);
+
+                StorageManagementService._ReplyMessageListStatic.Insert(
+                    0,
+                    new TextMessageObject { Text = "發生錯誤: 此欄位出現問題 " + ErrorText }
+                );
+
+                return;
             }
             else
             {
+                //? 確認寫法有沒有問題
+                //? 整理
+                //! 判斷是否為空
+                //! 個別處理
+                //! alttext 功用
+
+                var SearchStorageItemData =
+                    await _storageManagementDatabaseService.SearchStorageList(
+                        StorageInfo,
+                        WebHookEventDto.Source.UserId
+                    );
+                if (SearchStorageItemData.Count == 0)
+                {
+                    _ReplyMessageListStatic = new List<object>
+                    {
+                        new TextMessageObject { Text = "找不到此物品" }
+                    };
+                }
+                else
+                {
+                    _ReplyMessageListStatic = new List<object>
+                    {
+                        new FlexMessageObject<FlexCarouselContainer>
+                        {
+                            AltText = "Display Temporary Input",
+                            Contents = new FlexCarouselContainer
+                            {
+                                Type = FlexContainerTypeEnum.Carousel,
+                                Contents = new List<FlexBubbleContainer>
+                                {
+                                    new FlexBubbleContainer
+                                    {
+                                        Type = FlexContainerTypeEnum.Bubble,
+                                        Body = new FlexComponent
+                                        {
+                                            Type = FlexComponentTypeEnum.Box,
+                                            Layout = FlexComponentLayoutTypeEnum.Vertical,
+
+                                            Contents = new List<FlexComponent>
+                                            {
+                                                new FlexComponent
+                                                {
+                                                    Type = FlexComponentTypeEnum.Box,
+                                                    Layout = FlexComponentLayoutTypeEnum.Horizontal,
+                                                    AlignItems = "center",
+                                                    Contents = new List<FlexComponent>
+                                                    {
+                                                        new FlexComponent
+                                                        {
+                                                            Type = FlexComponentTypeEnum.Text,
+                                                            Text =
+                                                                StorageManagementKeywordGroup.Place,
+                                                            Size = "xs",
+                                                        },
+                                                        new FlexComponent
+                                                        {
+                                                            Type = FlexComponentTypeEnum.Text,
+                                                            // Text = placeValueText,
+                                                            Text = "add",
+                                                            Size = "xl",
+                                                            Align = "end"
+                                                        }
+                                                    }
+                                                },
+                                                // new FlexComponent
+                                                // {
+                                                //     Type = FlexComponentTypeEnum.Box,
+                                                //     Layout = FlexComponentLayoutTypeEnum.Vertical,
+                                                //     Margin = "xxl",
+                                                //     Spacing = "sm",
+                                                //     Contents = FieldTable
+                                                // },
+                                                new FlexComponent
+                                                {
+                                                    Type = FlexComponentTypeEnum.Separator,
+                                                    Margin = "xxl"
+                                                },
+                                                new FlexComponent
+                                                {
+                                                    Type = FlexComponentTypeEnum.Box,
+                                                    Layout = FlexComponentLayoutTypeEnum.Vertical,
+                                                    Contents = new List<FlexComponent>
+                                                    {
+                                                        new FlexComponent
+                                                        {
+                                                            Type = FlexComponentTypeEnum.Button,
+                                                            Action = new ActionDto
+                                                            {
+                                                                Type = ActionTypeEnum.Message,
+                                                                Label = "新增",
+                                                                Text = "新增"
+                                                            }
+                                                        },
+                                                        new FlexComponent
+                                                        {
+                                                            Type = FlexComponentTypeEnum.Button,
+                                                            Action = new ActionDto
+                                                            {
+                                                                Type = ActionTypeEnum.Postback,
+                                                                Label = "修改",
+                                                                Data = "修改",
+                                                                InputOption =
+                                                                    PostbackInputOptionEnum.OpenKeyboard
+                                                            }
+                                                        },
+                                                        new FlexComponent
+                                                        {
+                                                            Type = FlexComponentTypeEnum.Button,
+                                                            Action = new ActionDto
+                                                            {
+                                                                Type = ActionTypeEnum.Message,
+                                                                Label = "取消新增",
+                                                                Text = "取消新增",
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    };
+                }
+
                 // StorageInfo 做 search
                 //產生 FlexMessage 供選擇
 
