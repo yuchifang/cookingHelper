f9ec4af8dc6637c0b22760ceb27e3262b2605838
fangfelipe <fangfelipe@gmail.com>
2024-07-29 21:31:12 +0800

    add StorageSearch class for StorageSearchService

---
+++ CookingHelper.Server/LineDtoService/LineBotService/StorageManagement/StorageSearch.cs
@@ -0,0 +1,33 @@
+using CookingHelper.Enum;
+using CookingHelper.LineDto;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace CookingHelper.LineDtoService;
+
+class StorageSearch : UIWithData
+{
+    public virtual void Init(IMemoryCache memoryCache) { }
+
+    public List<object> GetSearchUIBlock(List<StorageInfo> StorageInfoList)
+    {
+        var FlexCarouselContents = new List<FlexBubbleContainer>();
+
+        foreach (StorageInfo StorageInfo in StorageInfoList)
+        {
+            FlexCarouselContents.Add(GetFlexBubbleContainer(StorageInfo));
+        }
+
+        return new List<object>
+        {
+            new FlexMessageObject<FlexCarouselContainer>
+            {
+                AltText = "庫存搜尋結果",
+                Contents = new FlexCarouselContainer
+                {
+                    Type = FlexContainerTypeEnum.Carousel,
+                    Contents = FlexCarouselContents
+                }
+            }
+        };
+    }
+}
