c8f2554e1b9d7ef87f3e342f2efd135b7f3551e3
fangfelipe <fangfelipe@gmail.com>
2024-07-29 21:24:47 +0800

    add MemoryCache

--- CookingHelper.Server/Program.cs
+++ CookingHelper.Server/Program.cs
@@ -12,6 +12,7 @@ namespace CookingHelper.Server
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddMemoryCache();
             builder.Services.AddHttpClient();
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
