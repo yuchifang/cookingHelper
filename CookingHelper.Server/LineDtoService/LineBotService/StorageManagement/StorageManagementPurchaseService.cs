19c9d9830c4d089a07d02e405e79bf247b806ab5
fangfelipe <fangfelipe@gmail.com>
2024-07-31 11:00:32 +0800

    change class to Function

--- CookingHelper.Server/LineDtoService/LineBotService/StorageManagement/StorageManagementPurchaseService.cs
+++ CookingHelper.Server/LineDtoService/LineBotService/StorageManagement/StorageManagementPurchaseService.cs
@@ -26,299 +26,6 @@ public class StorageManagementPurchaseService
 
     private static dynamic _ReplyMessageListStatic = new List<object>();
 
-    class InitStatus : StorageInputStatus
-    {
-        public override void Init()
-        {
-            LineBotService._WebhookEventStatusStatic = "新增物品至庫存";
-            _ReplyMessageListStatic = new List<object>(
-                [
-                    new TextMessageObject { Text = "依儲存位置,物品名稱,詳細位置,購買日期,過期日期輸入", },
-                    new TextMessageObject { Text = "儲存位置及物品名稱一定要填入, 沒填入將無法紀錄", },
-                    new TextMessageObject
-                    {
-                        Text = "請輸入儲存位置:",
-                        QuickReply = new QuickReplyItemDto
-                        {
-                            Items = new List<QuickReplyButtonDto>
-                            {
-                                GetQuickReplyButton(ActionTypeEnum.Message, "取消新增", "取消新增")
-                            }
-                        }
-                    }
-                ]
-            );
-            _InputStorageInfoStatic.Status = "place";
-        }
-    }
-
-    class PlaceStatus : StorageInputStatus
-    {
-        private readonly string _WebHookEventMessage;
-
-        public PlaceStatus(string WebHookEventMessage)
-        {
-            _WebHookEventMessage = WebHookEventMessage;
-        }
-
-        public override void Init()
-        {
-            _InputStorageInfoStatic.Place = _WebHookEventMessage;
-            _ReplyMessageListStatic = new List<object>(
-                [
-                    new TextMessageObject { Text = "此欄位為必填, 沒填入將無法紀錄", },
-                    new TextMessageObject
-                    {
-                        Text = "請輸入物品名稱:",
-                        QuickReply = new QuickReplyItemDto
-                        {
-                            Items = new List<QuickReplyButtonDto>
-                            {
-                                GetQuickReplyButton(ActionTypeEnum.Message, "取消新增", "取消新增")
-                            }
-                        }
-                    }
-                ]
-            );
-            _InputStorageInfoStatic.Status = "name";
-        }
-    }
-
-    class NameStatus : StorageInputStatus
-    {
-        private readonly string _WebHookEventMessage;
-
-        public NameStatus(string WebHookEventMessage)
-        {
-            _WebHookEventMessage = WebHookEventMessage;
-        }
-
-        public override void Init()
-        {
-            _InputStorageInfoStatic.Name = _WebHookEventMessage;
-            _ReplyMessageListStatic = new List<object>(
-                [
-                    new TextMessageObject
-                    {
-                        Text = "請輸入數量:",
-                        QuickReply = new QuickReplyItemDto
-                        {
-                            Items = new List<QuickReplyButtonDto>
-                            {
-                                GetQuickReplyButton(ActionTypeEnum.Message, "填寫完成", "填寫完成"),
-                                GetQuickReplyButton(ActionTypeEnum.Message, "略過", "略過"),
-                                GetQuickReplyButton(ActionTypeEnum.Message, "取消新增", "取消新增")
-                            }
-                        }
-                    }
-                ]
-            );
-            _InputStorageInfoStatic.Status = "amount";
-        }
-    }
-
-    class AmountStatus : StorageInputStatus
-    {
-        private readonly string? _WebHookEventMessage;
-
-        public AmountStatus(string? WebHookEventMessage)
-        {
-            _WebHookEventMessage = WebHookEventMessage;
-        }
-
-        public override void Init()
-        {
-            _InputStorageInfoStatic.Amount = _WebHookEventMessage;
-            _ReplyMessageListStatic = new List<object>(
-                [
-                    new TextMessageObject
-                    {
-                        Text = "請輸入詳細位置:",
-                        QuickReply = new QuickReplyItemDto
-                        {
-                            Items = new List<QuickReplyButtonDto>
-                            {
-                                GetQuickReplyButton(ActionTypeEnum.Message, "填寫完成", "填寫完成"),
-                                GetQuickReplyButton(ActionTypeEnum.Message, "略過", "略過"),
-                                GetQuickReplyButton(ActionTypeEnum.Message, "取消新增", "取消新增")
-                            }
-                        }
-                    }
-                ]
-            );
-            _InputStorageInfoStatic.Status = "location";
-        }
-    }
-
-    class LocationStatus : StorageInputStatus
-    {
-        private readonly string? _WebHookEventMessage;
-
-        public LocationStatus(string? WebHookEventMessage)
-        {
-            _WebHookEventMessage = WebHookEventMessage;
-        }
-
-        public override void Init()
-        {
-            _InputStorageInfoStatic.Location = _WebHookEventMessage;
-            _ReplyMessageListStatic = new List<object>(
-                [
-                    new TextMessageObject
-                    {
-                        Text = "請輸入購買日期(格式: YYYYMMDD):",
-                        QuickReply = new QuickReplyItemDto
-                        {
-                            Items = new List<QuickReplyButtonDto>
-                            {
-                                GetQuickReplyButton(ActionTypeEnum.Message, "填寫完成", "填寫完成"),
-                                GetQuickReplyButton(ActionTypeEnum.Message, "略過", "略過"),
-                                GetQuickReplyButton(ActionTypeEnum.Message, "取消新增", "取消新增")
-                            }
-                        }
-                    }
-                ]
-            );
-            _InputStorageInfoStatic.Status = "purchaseDate";
-        }
-    }
-
-    class PurchaseDateStatus : StorageInputStatus
-    {
-        private readonly string? _WebHookEventMessage;
-
-        public PurchaseDateStatus(string? WebHookEventMessage)
-        {
-            _WebHookEventMessage = WebHookEventMessage;
-        }
-
-        public override void Init()
-        {
-            var dateString = _WebHookEventMessage;
-
-            if (dateString == null)
-            {
-                _InputStorageInfoStatic.PurchaseDate = null;
-                _ReplyMessageListStatic = new List<object>(
-                    [
-                        new TextMessageObject
-                        {
-                            Text = "請輸入過期日期(格式: YYYYMMDD):",
-                            QuickReply = new QuickReplyItemDto
-                            {
-                                Items = new List<QuickReplyButtonDto>
-                                {
-                                    GetQuickReplyButton(ActionTypeEnum.Message, "填寫完成", "填寫完成"),
-                                    GetQuickReplyButton(ActionTypeEnum.Message, "略過", "略過"),
-                                    GetQuickReplyButton(ActionTypeEnum.Message, "取消新增", "取消新增")
-                                }
-                            }
-                        }
-                    ]
-                );
-                _InputStorageInfoStatic.Status = "expiryDate";
-            }
-            else if (DateOnly.TryParseExact(dateString, "yyyyMMdd", out DateOnly PurchaseDate))
-            {
-                _InputStorageInfoStatic.PurchaseDate = PurchaseDate;
-                _ReplyMessageListStatic = new List<object>(
-                    [
-                        new TextMessageObject
-                        {
-                            Text = "請輸入過期日期(格式: YYYYMMDD):",
-                            QuickReply = new QuickReplyItemDto
-                            {
-                                Items = new List<QuickReplyButtonDto>
-                                {
-                                    GetQuickReplyButton(ActionTypeEnum.Message, "填寫完成", "填寫完成"),
-                                    GetQuickReplyButton(ActionTypeEnum.Message, "略過", "略過"),
-                                    GetQuickReplyButton(ActionTypeEnum.Message, "取消新增", "取消新增")
-                                }
-                            }
-                        }
-                    ]
-                );
-                _InputStorageInfoStatic.Status = "expiryDate";
-            }
-            else
-            {
-                _ReplyMessageListStatic = DateTypeErrorHint("$ 購買日期格式錯誤, 請重新輸入(格式: YYYYMMDD)");
-            }
-        }
-    }
-
-    class ExpiryDateStatus : StorageInputStatus
-    {
-        private readonly string? _WebHookEventMessage;
-
-        public ExpiryDateStatus(string? WebHookEventMessage)
-        {
-            _WebHookEventMessage = WebHookEventMessage;
-        }
-
-        public override void Init()
-        {
-            var dateString = _WebHookEventMessage;
-            if (dateString == null)
-            {
-                _InputStorageInfoStatic.ExpiryDate = null;
-                _ReplyMessageListStatic = GetAdditionConfirmHint(_InputStorageInfoStatic);
-            }
-            else if (DateOnly.TryParseExact(dateString, "yyyyMMdd", out DateOnly ExpiryDate))
-            {
-                _InputStorageInfoStatic.ExpiryDate = ExpiryDate;
-                _ReplyMessageListStatic = GetAdditionConfirmHint(_InputStorageInfoStatic);
-            }
-            else
-            {
-                _ReplyMessageListStatic = DateTypeErrorHint("$ 過期日期格式錯誤, 請重新輸入(格式: YYYYMMDD)");
-            }
-        }
-    }
-
-    class EditStatus : StorageInputStatus
-    {
-        private readonly string? _WebHookEventMessage;
-
-        public EditStatus(string? WebHookEventMessage)
-        {
-            _WebHookEventMessage = WebHookEventMessage;
-        }
-
-        public override void Init()
-        {
-            StringToStorageInfo(
-                _WebHookEventMessage!,
-                out StorageInfo StorageInfoData,
-                out string ErrorText
-            );
-            if (ErrorText != "")
-            {
-                _ReplyMessageListStatic = GetAdditionConfirmHint(_InputStorageInfoStatic);
-
-                _ReplyMessageListStatic.Add(
-                    new TextMessageObject { Text = "發生錯誤: 此欄位出現問題 " + ErrorText }
-                );
-            }
-            else
-            {
-                if (StorageInfoData.Place != null)
-                    _InputStorageInfoStatic.Place = StorageInfoData.Place;
-                if (StorageInfoData.Name != null)
-                    _InputStorageInfoStatic.Name = StorageInfoData.Name;
-                if (StorageInfoData.Location != null)
-                    _InputStorageInfoStatic.Location = StorageInfoData.Location;
-                if (StorageInfoData.Amount != null)
-                    _InputStorageInfoStatic.Amount = StorageInfoData.Amount;
-                if (StorageInfoData.PurchaseDate != null)
-                    _InputStorageInfoStatic.PurchaseDate = StorageInfoData.PurchaseDate;
-                if (StorageInfoData.ExpiryDate != null)
-                    _InputStorageInfoStatic.ExpiryDate = StorageInfoData.ExpiryDate;
-                _ReplyMessageListStatic = GetAdditionConfirmHint(_InputStorageInfoStatic);
-            }
-        }
-    }
-
     public async Task InputStorage(WebhookEventDto WebHookEventDto)
     {
         string? WebHookEventMessage = WebHookEventDto.Message!.Text;
@@ -343,7 +50,7 @@ public class StorageManagementPurchaseService
             LineBotService._ReplyMessageRequestStatic = new ReplyMessageRequestDto<object>
             {
                 ReplyToken = WebHookEventDto.ReplyToken!,
-                Messages = new StorageInputStatus().GetAdditionConfirmHint(_InputStorageInfoStatic)
+                Messages = new InputStorageStatus().GetAdditionConfirmHint(_InputStorageInfoStatic)
             };
             return;
         }
@@ -364,19 +71,19 @@ public class StorageManagementPurchaseService
             return;
         }
 
-        var StatusProcessor = new Dictionary<string, StorageInputStatus>
+        var StatusProcessor = new Dictionary<string, Action>
         {
-            { "init", new InitStatus() },
-            { "place", new PlaceStatus(WebHookEventMessage!) },
-            { "name", new NameStatus(WebHookEventMessage!) },
-            { "amount", new AmountStatus(WebHookEventMessage) },
-            { "location", new LocationStatus(WebHookEventMessage) },
-            { "purchaseDate", new PurchaseDateStatus(WebHookEventMessage) },
-            { "expiryDate", new ExpiryDateStatus(WebHookEventMessage) },
-            { "edit", new EditStatus(WebHookEventMessage) }
+            { "init", InitStatus },
+            { "place", () => PlaceStatus(WebHookEventMessage!) },
+            { "name", () => NameStatus(WebHookEventMessage!) },
+            { "amount", () => AmountStatus(WebHookEventMessage!) },
+            { "location", () => LocationStatus(WebHookEventMessage!) },
+            { "purchaseDate", () => PurchaseDateStatus(WebHookEventMessage!) },
+            { "expiryDate", () => ExpiryDateStatus(WebHookEventMessage!) },
+            { "edit", () => EditStatus(WebHookEventMessage!) }
         };
 
-        StatusProcessor[_InputStorageInfoStatic.Status].Init();
+        StatusProcessor[_InputStorageInfoStatic.Status]();
 
         LineBotService._ReplyMessageRequestStatic = new ReplyMessageRequestDto<object>
         {
@@ -422,5 +129,165 @@ public class StorageManagementPurchaseService
             ReplyToken = WebHookEventDto.ReplyToken!,
             Messages = _ReplyMessageListStatic
         };
+    }
+
+    public void InitStatus()
+    {
+        var StorageStatus = InputStorageStatus.Instance;
+        LineBotService._WebhookEventStatusStatic = "新增物品至庫存";
+        _ReplyMessageListStatic = new List<object>(
+            [
+                new TextMessageObject { Text = "依儲存位置,物品名稱,詳細位置,購買日期,有效日期輸入", },
+                new TextMessageObject { Text = "儲存位置及物品名稱一定要填入, 沒填入將無法紀錄", },
+                new TextMessageObject
+                {
+                    Text = "請輸入儲存位置:",
+                    QuickReply = new QuickReplyItemDto
+                    {
+                        Items = new List<QuickReplyButtonDto>
+                        {
+                            StorageStatus.GetQuickReplyButton(
+                                ActionTypeEnum.Message,
+                                "取消新增",
+                                "取消新增"
+                            )
+                        }
+                    }
+                }
+            ]
+        );
+        _InputStorageInfoStatic.Status = "place";
+    }
+
+    public void PlaceStatus(string WebHookEventMessage)
+    {
+        var StorageStatus = InputStorageStatus.Instance;
+        _InputStorageInfoStatic.Place = WebHookEventMessage;
+        _ReplyMessageListStatic = new List<object>(
+            [
+                new TextMessageObject { Text = "此欄位為必填, 沒填入將無法紀錄", },
+                new TextMessageObject
+                {
+                    Text = "請輸入物品名稱:",
+                    QuickReply = new QuickReplyItemDto
+                    {
+                        Items = new List<QuickReplyButtonDto>
+                        {
+                            StorageStatus.GetQuickReplyButton(
+                                ActionTypeEnum.Message,
+                                "取消新增",
+                                "取消新增"
+                            )
+                        }
+                    }
+                }
+            ]
+        );
+        _InputStorageInfoStatic.Status = "name";
+    }
+
+    public void NameStatus(string WebHookEventMessage)
+    {
+        var StorageStatus = InputStorageStatus.Instance;
+        _InputStorageInfoStatic.Name = WebHookEventMessage;
+        _ReplyMessageListStatic = StorageStatus.GetRegularReply("請輸入數量:");
+        _InputStorageInfoStatic.Status = "amount";
+    }
+
+    public void AmountStatus(string WebHookEventMessage)
+    {
+        var StorageStatus = InputStorageStatus.Instance;
+        _InputStorageInfoStatic.Amount = WebHookEventMessage;
+        _ReplyMessageListStatic = StorageStatus.GetRegularReply("請輸入詳細位置:");
+        _InputStorageInfoStatic.Status = "location";
+    }
+
+    public void LocationStatus(string WebHookEventMessage)
+    {
+        var StorageStatus = InputStorageStatus.Instance;
+        _InputStorageInfoStatic.Location = WebHookEventMessage;
+        _ReplyMessageListStatic = StorageStatus.GetRegularReply("請輸入購買日期(格式: YYYYMMDD):");
+        _InputStorageInfoStatic.Status = "purchaseDate";
+    }
+
+    public void PurchaseDateStatus(string WebHookEventMessage)
+    {
+        var StorageStatus = InputStorageStatus.Instance;
+        var dateString = WebHookEventMessage;
+
+        _ReplyMessageListStatic = StorageStatus.GetRegularReply("請輸入有效日期(格式: YYYYMMDD):");
+
+        if (dateString == null)
+        {
+            _InputStorageInfoStatic.PurchaseDate = null;
+            _InputStorageInfoStatic.Status = "expiryDate";
+        }
+        else if (DateOnly.TryParseExact(dateString, "yyyyMMdd", out DateOnly PurchaseDate))
+        {
+            _InputStorageInfoStatic.PurchaseDate = PurchaseDate;
+            _InputStorageInfoStatic.Status = "expiryDate";
+        }
+        else
+        {
+            _ReplyMessageListStatic = StorageStatus.DateTypeErrorHint(
+                "$ 購買日期格式錯誤, 請重新輸入(格式: YYYYMMDD)"
+            );
+        }
+    }
+
+    public void ExpiryDateStatus(string WebHookEventMessage)
+    {
+        var StorageStatus = InputStorageStatus.Instance;
+        var dateString = WebHookEventMessage;
+        if (dateString == null)
+        {
+            _InputStorageInfoStatic.ExpiryDate = null;
+            _ReplyMessageListStatic = StorageStatus.GetAdditionConfirmHint(_InputStorageInfoStatic);
+        }
+        else if (DateOnly.TryParseExact(dateString, "yyyyMMdd", out DateOnly ExpiryDate))
+        {
+            _InputStorageInfoStatic.ExpiryDate = ExpiryDate;
+            _ReplyMessageListStatic = StorageStatus.GetAdditionConfirmHint(_InputStorageInfoStatic);
+        }
+        else
+        {
+            _ReplyMessageListStatic = StorageStatus.DateTypeErrorHint(
+                "$ 有效日期格式錯誤, 請重新輸入(格式: YYYYMMDD)"
+            );
+        }
+    }
+
+    public void EditStatus(string WebHookEventMessage)
+    {
+        var StorageStatus = InputStorageStatus.Instance;
+        StringToStorageInfo(
+            WebHookEventMessage!,
+            out StorageInfo StorageInfoData,
+            out string ErrorText
+        );
+        if (ErrorText != "")
+        {
+            _ReplyMessageListStatic = StorageStatus.GetAdditionConfirmHint(_InputStorageInfoStatic);
+
+            _ReplyMessageListStatic.Add(
+                new TextMessageObject { Text = "發生錯誤: 此欄位出現問題 " + ErrorText }
+            );
+        }
+        else
+        {
+            if (StorageInfoData.Place != null)
+                _InputStorageInfoStatic.Place = StorageInfoData.Place;
+            if (StorageInfoData.Name != null)
+                _InputStorageInfoStatic.Name = StorageInfoData.Name;
+            if (StorageInfoData.Location != null)
+                _InputStorageInfoStatic.Location = StorageInfoData.Location;
+            if (StorageInfoData.Amount != null)
+                _InputStorageInfoStatic.Amount = StorageInfoData.Amount;
+            if (StorageInfoData.PurchaseDate != null)
+                _InputStorageInfoStatic.PurchaseDate = StorageInfoData.PurchaseDate;
+            if (StorageInfoData.ExpiryDate != null)
+                _InputStorageInfoStatic.ExpiryDate = StorageInfoData.ExpiryDate;
+            _ReplyMessageListStatic = StorageStatus.GetAdditionConfirmHint(_InputStorageInfoStatic);
+        }
     }
 }
