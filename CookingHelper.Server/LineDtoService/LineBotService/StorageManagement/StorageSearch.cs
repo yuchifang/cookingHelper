4a2a83a92206f749f91339e66e0f2c43270fb034
fangfelipe <fangfelipe@gmail.com>
2024-08-01 11:52:30 +0800

    change name, add new Function

--- CookingHelper.Server/LineDtoService/LineBotService/StorageManagement/StorageSearch.cs
+++ CookingHelper.Server/LineDtoService/LineBotService/StorageManagement/StorageSearch.cs
@@ -1,14 +1,16 @@
 using CookingHelper.Enum;
 using CookingHelper.LineDto;
 using Microsoft.Extensions.Caching.Memory;
+using static CookingHelper.LineDto.BaseMessageObject;
+using static CookingHelper.Utils;
 
 namespace CookingHelper.LineDtoService;
 
-public class StorageSearch : UIWithData
+public class StorageSearchBaseStruct : UIWithData
 {
-    public virtual void Init(IMemoryCache memoryCache) { }
+    public static StorageSearchBaseStruct Instance = new StorageSearchBaseStruct();
 
-    public List<object> GetSearchUIBlock(List<StorageInfo> StorageInfoList)
+    public List<object> GetSearchResultItemList(List<StorageInfo> StorageInfoList)
     {
         var FlexCarouselContents = new List<FlexBubbleContainer>();
 
@@ -29,5 +31,39 @@ public class StorageSearch : UIWithData
                 }
             }
         };
+    }
+
+    public List<object> GetSearchResultUIBlock(
+        IEnumerable<StorageInfo> StorageInfoEnumerable,
+        int PageIndex,
+        int PageSize
+    )
+    {
+        var SearchedStoreItemEnumerable = Paginate(
+            StorageInfoEnumerable,
+            PageIndex,
+            PageSize,
+            out bool hasNextPage,
+            out bool hasPrevPage
+        );
+
+        var SearchGroup = GetSearchResultItemList(SearchedStoreItemEnumerable.ToList());
+
+        if (hasNextPage && hasPrevPage)
+        {
+            ((FlexMessageObject<FlexCarouselContainer>)SearchGroup[0]).QuickReply =
+                GetPrevAndNextPageQuickItem();
+        }
+        else if (hasNextPage)
+        {
+            ((FlexMessageObject<FlexCarouselContainer>)SearchGroup[0]).QuickReply =
+                GetNextPageQuickItem();
+        }
+        else if (hasPrevPage)
+        {
+            ((FlexMessageObject<FlexCarouselContainer>)SearchGroup[0]).QuickReply =
+                GetPrevPageQuickItem();
+        }
+        return SearchGroup;
     }
 }
