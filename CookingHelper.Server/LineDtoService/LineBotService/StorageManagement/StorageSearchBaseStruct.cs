ef9457d5fe201c99b6cc95cf28e0998ca6052ccf
fangfelipe <fangfelipe@gmail.com>
2024-08-02 21:37:02 +0800

    change Type, chage Name

--- CookingHelper.Server/LineDtoService/LineBotService/StorageManagement/StorageSearchBaseStruct.cs
+++ CookingHelper.Server/LineDtoService/LineBotService/StorageManagement/StorageSearchBaseStruct.cs
@@ -1,5 +1,7 @@
+using System.Text.Json;
 using CookingHelper.Enum;
 using CookingHelper.LineDto;
+using CookingHelper.Model;
 using Microsoft.Extensions.Caching.Memory;
 using static CookingHelper.LineDto.BaseMessageObject;
 using static CookingHelper.Utils;
@@ -10,13 +12,74 @@ public class StorageSearchBaseStruct : UIWithData
 {
     public static StorageSearchBaseStruct Instance = new StorageSearchBaseStruct();
 
-    public List<object> GetSearchResultItemList(List<StorageInfo> StorageInfoList)
+    public FlexBubbleContainer GetFlexBubbleContainer(StoreItem StoreItem)
+    {
+        var StorageInfoTable = GetStorageInfoTable(StoreItem);
+        var StorageTable = new List<FlexComponent>
+        {
+            new FlexComponent { Type = FlexComponentTypeEnum.Separator, Margin = "xxl" },
+            new FlexComponent
+            {
+                Type = FlexComponentTypeEnum.Box,
+                Layout = FlexComponentLayoutTypeEnum.Horizontal,
+                Contents = new List<FlexComponent>
+                {
+                    new FlexComponent
+                    {
+                        Type = FlexComponentTypeEnum.Button,
+                        Action = new ActionDto
+                        {
+                            Type = ActionTypeEnum.Postback,
+                            Label = "刪除",
+                            Data = "c" + JsonSerializer.Serialize(StoreItem),
+                            DisplayText = "刪除",
+                        }
+                    },
+                    new FlexComponent
+                    {
+                        Type = FlexComponentTypeEnum.Button,
+                        Action = new ActionDto
+                        {
+                            Type = ActionTypeEnum.Postback,
+                            Label = "修改",
+                            Data = "修改",
+                            InputOption = PostbackInputOptionEnum.OpenKeyboard
+                        }
+                    },
+                    new FlexComponent
+                    {
+                        Type = FlexComponentTypeEnum.Button,
+                        Action = new ActionDto
+                        {
+                            Type = ActionTypeEnum.Message,
+                            Label = "返回",
+                            Text = "返回",
+                        }
+                    }
+                }
+            }
+        };
+        StorageTable.InsertRange(0, StorageInfoTable);
+        return new FlexBubbleContainer
+        {
+            Type = FlexContainerTypeEnum.Bubble,
+            Body = new FlexComponent
+            {
+                Type = FlexComponentTypeEnum.Box,
+                Layout = FlexComponentLayoutTypeEnum.Vertical,
+
+                Contents = StorageTable
+            }
+        };
+    }
+
+    public List<object> GetSearchResultItemList(List<StoreItem> StoreItemList)
     {
         var FlexCarouselContents = new List<FlexBubbleContainer>();
 
-        foreach (StorageInfo StorageInfo in StorageInfoList)
+        foreach (StoreItem StoreItem in StoreItemList)
         {
-            FlexCarouselContents.Add(GetFlexBubbleContainer(StorageInfo));
+            FlexCarouselContents.Add(GetFlexBubbleContainer(StoreItem));
         }
 
         return new List<object>
@@ -34,20 +97,20 @@ public class StorageSearchBaseStruct : UIWithData
     }
 
     public List<object> GetSearchResultUIBlock(
-        IQueryable<StorageInfo> StorageInfoEnumerable,
+        IQueryable<StoreItem> StorageInfoQueryable,
         int PageIndex,
         int PageSize
     )
     {
-        var SearchedStoreItemEnumerable = Paginate(
-            StorageInfoEnumerable,
+        var SearchedStoreItemQueryable = Paginate(
+            StorageInfoQueryable,
             PageIndex,
             PageSize,
             out bool hasNextPage,
             out bool hasPrevPage
         );
 
-        var SearchGroup = GetSearchResultItemList(SearchedStoreItemEnumerable.ToList());
+        var SearchGroup = GetSearchResultItemList(SearchedStoreItemQueryable.ToList());
 
         if (hasNextPage && hasPrevPage)
         {
