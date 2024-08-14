c14278484125cf2e10b826bef5cd08a726d09819
fangfelipe <fangfelipe@gmail.com>
2024-08-14 22:31:51 +0800

    change name

--- CookingHelper.Server/Program.cs
+++ CookingHelper.Server/Program.cs
@@ -27,7 +27,7 @@ namespace CookingHelper.Server
             builder.Services.AddScoped<LineBotService, LineBotService>();
             builder.Services.AddScoped<RichMenuService, RichMenuService>();
             builder.Services.AddScoped<ShoppingListDatabaseService, ShoppingListDatabaseService>();
-            builder.Services.AddScoped<ShoppingListLogicService, ShoppingListLogicService>();
+            builder.Services.AddScoped<ShoppingListService, ShoppingListService>();
             builder.Services.AddScoped<StorageManagementService, StorageManagementService>();
             builder.Services.AddScoped<
                 StorageManagementPurchaseService,
