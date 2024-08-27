eb1606c947a9f6509abaa3adf5b2276f95d406e9
fangfelipe <fangfelipe@gmail.com>
2024-08-27 14:57:12 +0800

    add NameStatus,ImageContentStatus, InputImageError, IngredientsStatus

--- CookingHelper.Server/LineDtoService/LineBotService/RecipeList/RecipeListAddition.cs
+++ CookingHelper.Server/LineDtoService/LineBotService/RecipeList/RecipeListAddition.cs
@@ -1,3 +1,4 @@
+using System.Net.Http.Headers;
 using CookingHelper.Enum;
 using CookingHelper.LineDto;
 using CookingHelper.LineDtoService;
@@ -11,8 +12,21 @@ public class RecipeListAddition
 
     private readonly RecipeListService _recipeListService;
 
-    public RecipeListAddition(RecipeListService RecipeListService)
+    private readonly HttpClient _client;
+    private readonly IHttpClientFactory _httpClientFactory;
+    private readonly IConfiguration _configuration;
+
+    private readonly string getContentUri = "https://api-data.line.me/v2/bot/message/{0}/content";
+
+    public RecipeListAddition(
+        RecipeListService RecipeListService,
+        IHttpClientFactory httpClientFactory,
+        IConfiguration configuration
+    )
     {
+        _configuration = configuration;
+        _httpClientFactory = httpClientFactory;
+        _client = _httpClientFactory.CreateClient();
         _recipeListService = RecipeListService;
     }
 
@@ -20,6 +34,11 @@ public class RecipeListAddition
     public async Task InputRecipeList(WebhookEventDto WebHookEventDto)
     {
         string? WebHookEventMessage = WebHookEventDto.Message!.Text;
+        if (WebHookEventMessage == "新增食譜")
+        {
+            _InputRecipeInfoStatic = new InputRecipeInfo();
+        }
+
         if (WebHookEventMessage == "取消新增")
         {
             _InputRecipeInfoStatic = new InputRecipeInfo();
@@ -29,7 +48,9 @@ public class RecipeListAddition
         var StatusProcessor = new Dictionary<string, Action>
         {
             { "Init", InitStatus },
-            { "Name", () => NameStatus(WebHookEventMessage!) }
+            { "Name", () => NameStatus(WebHookEventMessage!) },
+            { "ImageContent", InputImageError },
+            { "Ingredients", () => IngredientsStatus(WebHookEventMessage!) }
         };
         StatusProcessor[_InputRecipeInfoStatic.Status]();
 
@@ -47,7 +68,7 @@ public class RecipeListAddition
         _ReplyMessageListStatic = new List<object>(
             [
                 new TextMessageObject { Text = "依食譜名稱,圖片,食材,步驟輸入", },
-                new TextMessageObject { Text = "食譜名稱一定要填入, 沒填入將無法紀錄", },
+                new TextMessageObject { Text = "食譜名稱,食材,步驟一定要填入, 沒填入將無法紀錄", },
                 new TextMessageObject
                 {
                     Text = "請輸入食譜名稱:",
@@ -72,19 +93,156 @@ public class RecipeListAddition
     {
         var StorageStatus = InputStorageBaseClass.Instance;
         _InputRecipeInfoStatic.Name = WebHookEventMessage;
-        _ReplyMessageListStatic = StorageStatus.GetRegularReply("請輸入圖片:");
+        _ReplyMessageListStatic = new List<object>(
+            [
+                new TextMessageObject
+                {
+                    Text = "請輸入食譜圖片: (限制4MB)",
+                    QuickReply = new QuickReplyItemDto
+                    {
+                        Items = new List<QuickReplyButtonDto>
+                        {
+                            StorageStatus.GetQuickReplyButton(
+                                ActionTypeEnum.Message,
+                                "取消新增",
+                                "取消新增"
+                            ),
+                            StorageStatus.GetQuickReplyButton(ActionTypeEnum.Message, "略過", "略過")
+                        }
+                    }
+                }
+            ]
+        );
         _InputRecipeInfoStatic.Status = "ImageContent";
     }
 
-    //! 建立 Image webhook
-
     //! update Database
-    //! 使用下面的
-    //! 限制大小長寬寬度??
-    //! 轉 byte[] 存 _InputRecipeInfoStatic
-    public async Task ImageContentStatus(WebhookEventDto WebHookEventDto)
+    public async Task ImageContentStatusImageEvent(WebhookEventDto WebHookEventDto)
     {
-        Console.WriteLine(WebHookEventDto);
+        var messageId = WebHookEventDto.Message!.Id;
+
+        var request = new HttpRequestMessage(
+            HttpMethod.Get,
+            String.Format(getContentUri, messageId)
+        );
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
+            "Bearer",
+            _configuration["LineBot:ChannelAccessToken"]
+        );
+
+        var response = await _client.SendAsync(request).ConfigureAwait(false);
+        response.EnsureSuccessStatusCode();
+        var image = await response.Content.ReadAsByteArrayAsync();
+        var StorageStatus = InputStorageBaseClass.Instance;
+        if (image.Length > 4 * 1024 * 1024)
+        {
+            _ReplyMessageListStatic = new List<object>(
+                [
+                    new TextMessageObject { Text = "超過限制大小" },
+                    new TextMessageObject
+                    {
+                        Text = "請輸入食譜圖片: (限制4MB)",
+                        QuickReply = new QuickReplyItemDto
+                        {
+                            Items = new List<QuickReplyButtonDto>
+                            {
+                                StorageStatus.GetQuickReplyButton(
+                                    ActionTypeEnum.Message,
+                                    "取消新增",
+                                    "取消新增"
+                                ),
+                                StorageStatus.GetQuickReplyButton(
+                                    ActionTypeEnum.Message,
+                                    "略過",
+                                    "略過"
+                                )
+                            }
+                        }
+                    }
+                ]
+            );
+        }
+        else
+        {
+            _InputRecipeInfoStatic.ImageContent = image;
+            _ReplyMessageListStatic = new List<object>(
+                [
+                    new TextMessageObject
+                    {
+                        Text = "請輸入食材:",
+                        QuickReply = new QuickReplyItemDto
+                        {
+                            Items = new List<QuickReplyButtonDto>
+                            {
+                                StorageStatus.GetQuickReplyButton(
+                                    ActionTypeEnum.Message,
+                                    "取消新增",
+                                    "取消新增"
+                                ),
+                            }
+                        }
+                    }
+                ]
+            );
+            _InputRecipeInfoStatic.Status = "Ingredients";
+        }
+        LineBotService._ReplyMessageRequestStatic = new ReplyMessageRequestDto<object>
+        {
+            ReplyToken = WebHookEventDto.ReplyToken!,
+            Messages = _ReplyMessageListStatic
+        };
+    }
+
+    public void InputImageError()
+    {
+        var StorageStatus = InputStorageBaseClass.Instance;
+        _ReplyMessageListStatic = new List<object>(
+            [
+                new TextMessageObject { Text = "輸入錯誤" },
+                new TextMessageObject
+                {
+                    Text = "請輸入食譜圖片: (限制4MB)",
+                    QuickReply = new QuickReplyItemDto
+                    {
+                        Items = new List<QuickReplyButtonDto>
+                        {
+                            StorageStatus.GetQuickReplyButton(
+                                ActionTypeEnum.Message,
+                                "取消新增",
+                                "取消新增"
+                            ),
+                            StorageStatus.GetQuickReplyButton(ActionTypeEnum.Message, "略過", "略過")
+                        }
+                    }
+                }
+            ]
+        );
+    }
+
+    public void IngredientsStatus(string WebHookEventMessage)
+    {
+        var StorageStatus = InputStorageBaseClass.Instance;
+        _InputRecipeInfoStatic.Ingredients = WebHookEventMessage;
+        _ReplyMessageListStatic = new List<object>(
+            [
+                new TextMessageObject
+                {
+                    Text = "請輸入步驟",
+                    QuickReply = new QuickReplyItemDto
+                    {
+                        Items = new List<QuickReplyButtonDto>
+                        {
+                            StorageStatus.GetQuickReplyButton(
+                                ActionTypeEnum.Message,
+                                "取消新增",
+                                "取消新增"
+                            )
+                        }
+                    }
+                }
+            ]
+        );
+        _InputRecipeInfoStatic.Status = "Step";
     }
 }
 
@@ -96,7 +254,13 @@ public class InputRecipeInfo : RecipeItem
         get => _Status;
         set
         {
-            if (value != "Init" && value != "Name" && value != "ImageContent" && value != "Step")
+            if (
+                value != "Init"
+                && value != "Name"
+                && value != "ImageContent"
+                && value != "Step"
+                && value != "Ingredients"
+            )
             {
                 throw new ArgumentException("Value Error");
             }
