73e5ebf954e52f4a9a5631c7942ce92764c1e2a9
fangfelipe <fangfelipe@gmail.com>
2024-08-29 19:07:51 +0800

    add migrations

---
+++ CookingHelper.Server/Migrations/20240829081531_changeimagepath.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CookingHelper.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class changeimagepath : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageContent",
+                table: "RecipeItem");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePath",
+                table: "RecipeItem",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImagePath",
+                table: "RecipeItem");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "ImageContent",
+                table: "RecipeItem",
+                type: "longblob",
+                nullable: false);
+        }
+    }
+}
