dec925307a9f5fa9ed778ed13d038166eddbc83f
fangfelipe <fangfelipe@gmail.com>
2024-10-10 16:49:56 +0800

    package.json script, change vite.config.ts

--- cookinghelper.client/vite.config.ts
+++ cookinghelper.client/vite.config.ts
@@ -23,16 +23,18 @@ const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
 const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
 
 if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
-    if (0 !== child_process.spawnSync('dotnet', [
-        'dev-certs',
-        'https',
-        '--export-path',
-        certFilePath,
-        '--format',
-        'Pem',
-        '--no-password',
-    ], { stdio: 'inherit', }).status) {
-        throw new Error("Could not create certificate.");
+    try {
+        child_process.spawnSync('dotnet', [
+            'dev-certs',
+            'https',
+            '--export-path',
+            certFilePath,
+            '--format',
+            'Pem',
+            '--no-password',
+        ], { stdio: 'inherit' });
+    } catch (error) {
+        console.warn("Could not create certificate. Continuing without HTTPS support.");
     }
 }
 
