a1f19c1d301510ff3c610ccf58074682f0f77abe
fangfelipe <fangfelipe@gmail.com>
2024-10-29 16:59:54 +0800

    test pre-commit

--- cookinghelper.client/src/SingInPage/SingInPage.tsx
+++ cookinghelper.client/src/SingInPage/SingInPage.tsx
@@ -1,19 +1,19 @@
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Checkbox from '@mui/material/Checkbox';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormLabel from '@mui/material/FormLabel';
-import FormControl from '@mui/material/FormControl';
-import Link from '@mui/material/Link';
-import TextField from '@mui/material/TextField';
-import Typography from '@mui/material/Typography';
-import Stack from '@mui/material/Stack';
-import MuiCard from '@mui/material/Card';
-import { styled } from '@mui/material/styles';
-import ForgotPassword from '../ForgotPassword';
-import { Params, useActionData, useFetcher } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import Alert from '@mui/material/Alert';
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Checkbox from "@mui/material/Checkbox";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormLabel from "@mui/material/FormLabel";
+import FormControl from "@mui/material/FormControl";
+import Link from "@mui/material/Link";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
+import MuiCard from "@mui/material/Card";
+import { styled } from "@mui/material/styles";
+import ForgotPassword from "../ForgotPassword";
+import { Params, useActionData, useFetcher } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Alert from "@mui/material/Alert";
 /*
    ? FormData 是甚麼   
     
@@ -25,94 +25,98 @@ import Alert from '@mui/material/Alert';
 
     換頁
     SingInPage
+
+    測試
 */
 
-export async function action({ params, request }: { params: Params, request: Request }) {
+export async function action({
+  params,
+  request,
+}: {
+  params: Params;
+  request: Request;
+}) {
   let formData: FormData = await request.formData();
 
   const response = await fetch("api/account/login", {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify({ email: formData.get("email"), password: formData.get("password") })
+    body: JSON.stringify({
+      email: formData.get("email"),
+      password: formData.get("password"),
+    }),
   });
   const responseData = await response.json();
 
   if (!response.ok) {
-
     return responseData;
   } else {
-
   }
 
   console.log("data", responseData);
   // 做換頁
 }
 
-
-
-
 const Card = styled(MuiCard)(({ theme }) => ({
-  display: 'flex',
-  flexDirection: 'column',
-  alignSelf: 'center',
-  width: '100%',
+  display: "flex",
+  flexDirection: "column",
+  alignSelf: "center",
+  width: "100%",
   padding: theme.spacing(4),
   gap: theme.spacing(2),
-  margin: 'auto',
-  [theme.breakpoints.up('sm')]: {
-    maxWidth: '450px',
+  margin: "auto",
+  [theme.breakpoints.up("sm")]: {
+    maxWidth: "450px",
   },
   boxShadow:
-    'hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px',
-  ...theme.applyStyles('dark', {
+    "hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px",
+  ...theme.applyStyles("dark", {
     boxShadow:
-      'hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px',
+      "hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px",
   }),
 }));
 
 const SignInContainer = styled(Stack)(({ theme }) => ({
-  minHeight: '100%',
+  minHeight: "100%",
   minWidth: "500px",
   padding: theme.spacing(2),
-  [theme.breakpoints.up('sm')]: {
+  [theme.breakpoints.up("sm")]: {
     padding: theme.spacing(4),
   },
-  '&::before': {
+  "&::before": {
     content: '""',
-    display: 'block',
-    position: 'absolute',
+    display: "block",
+    position: "absolute",
     zIndex: -1,
     inset: 0,
     backgroundImage:
-      'radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))',
-    backgroundRepeat: 'no-repeat',
-    ...theme.applyStyles('dark', {
+      "radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))",
+    backgroundRepeat: "no-repeat",
+    ...theme.applyStyles("dark", {
       backgroundImage:
-        'radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))',
+        "radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))",
     }),
   },
 }));
 
 export function SignInPage(props: { disableCustomTheme?: boolean }) {
   const [emailError, setEmailError] = useState(false);
-  const [emailErrorMessage, setEmailErrorMessage] = useState('');
+  const [emailErrorMessage, setEmailErrorMessage] = useState("");
   const [passwordError, setPasswordError] = useState(false);
-  const [passwordErrorMessage, setPasswordErrorMessage] = useState('');
+  const [passwordErrorMessage, setPasswordErrorMessage] = useState("");
   const [open, setOpen] = useState(false);
   let fetcher = useFetcher();
   const response = fetcher.data;
-
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const submitFormData = new FormData(event.currentTarget);
     if (validateInputs()) {
-      fetcher.submit(submitFormData, { method: "post", action: "/" })
+      fetcher.submit(submitFormData, { method: "post", action: "/" });
     }
-  }
-
+  };
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -122,30 +126,28 @@ export function SignInPage(props: { disableCustomTheme?: boolean }) {
     setOpen(false);
   };
 
-
-
   const validateInputs = () => {
-    const email = document.getElementById('email') as HTMLInputElement;
-    const password = document.getElementById('password') as HTMLInputElement;
+    const email = document.getElementById("email") as HTMLInputElement;
+    const password = document.getElementById("password") as HTMLInputElement;
 
     let isValid = true;
 
     if (!email.value || !/\S+@\S+\.\S+/.test(email.value)) {
       setEmailError(true);
-      setEmailErrorMessage('Please enter a valid email address.');
+      setEmailErrorMessage("Please enter a valid email address.");
       isValid = false;
     } else {
       setEmailError(false);
-      setEmailErrorMessage('');
+      setEmailErrorMessage("");
     }
 
     if (!password.value || password.value.length < 6) {
       setPasswordError(true);
-      setPasswordErrorMessage('Password must be at least 6 characters long.');
+      setPasswordErrorMessage("Password must be at least 6 characters long.");
       isValid = false;
     } else {
       setPasswordError(false);
-      setPasswordErrorMessage('');
+      setPasswordErrorMessage("");
     }
 
     return isValid;
@@ -158,21 +160,21 @@ export function SignInPage(props: { disableCustomTheme?: boolean }) {
           <Typography
             component="h1"
             variant="h4"
-            sx={{ width: '100%', fontSize: 'clamp(2rem, 10vw, 2.15rem)' }}
+            sx={{ width: "100%", fontSize: "clamp(2rem, 10vw, 2.15rem)" }}
           >
             Sign in
           </Typography>
-          <fetcher.Form method="post" onSubmit={handleSubmit} >
+          <fetcher.Form method="post" onSubmit={handleSubmit}>
             <Box
               sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                width: '100%',
+                display: "flex",
+                flexDirection: "column",
+                width: "100%",
                 gap: 2,
               }}
             >
               <FormControl>
-                <Box sx={{ display: 'flex', justifyContent: 'start' }}>
+                <Box sx={{ display: "flex", justifyContent: "start" }}>
                   <FormLabel htmlFor="email">Email</FormLabel>
                 </Box>
                 <TextField
@@ -187,19 +189,19 @@ export function SignInPage(props: { disableCustomTheme?: boolean }) {
                   required
                   fullWidth
                   variant="outlined"
-                  color={emailError ? 'error' : 'primary'}
-                  sx={{ ariaLabel: 'email' }}
+                  color={emailError ? "error" : "primary"}
+                  sx={{ ariaLabel: "email" }}
                 />
               </FormControl>
               <FormControl>
-                <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                <Box sx={{ display: "flex", justifyContent: "space-between" }}>
                   <FormLabel htmlFor="password">Password</FormLabel>
                   <Link
                     component="button"
                     type="button"
                     onClick={handleClickOpen}
                     variant="body2"
-                    sx={{ alignSelf: 'baseline' }}
+                    sx={{ alignSelf: "baseline" }}
                   >
                     Forgot your password?
                   </Link>
@@ -216,16 +218,14 @@ export function SignInPage(props: { disableCustomTheme?: boolean }) {
                   required
                   fullWidth
                   variant="outlined"
-                  color={passwordError ? 'error' : 'primary'}
+                  color={passwordError ? "error" : "primary"}
                 />
               </FormControl>
               <FormControlLabel
                 control={<Checkbox value="remember" color="primary" />}
                 label="Remember me"
               />
-              {response && <Alert severity="error">
-                {response.message}
-              </Alert>}
+              {response && <Alert severity="error">{response.message}</Alert>}
               <ForgotPassword open={open} handleClose={handleClose} />
               <Button
                 type="submit"
