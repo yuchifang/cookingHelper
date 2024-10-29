ddcce7de2acf413d01cd1b89e2fc7ea17d34e7f7
fangfelipe <fangfelipe@gmail.com>
2024-10-29 21:25:40 +0800

    test pre commit

--- cookinghelper.client/src/ForgotPassword.tsx
+++ cookinghelper.client/src/ForgotPassword.tsx
@@ -6,11 +6,9 @@ import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 import OutlinedInput from "@mui/material/OutlinedInput";
-// test
 
 interface ForgotPasswordProps {
   open: boolean;
-  data?: () => void;
   handleClose: () => void;
 }
 
