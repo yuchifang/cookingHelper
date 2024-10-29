05dd2262c510150fd94e5a23f3d2a039f1024a3f
fangfelipe <fangfelipe@gmail.com>
2024-10-29 10:48:59 +0800

    test git pre-commit

--- cookinghelper.client/src/SingInPage/SingInPage.tsx
+++ cookinghelper.client/src/SingInPage/SingInPage.tsx
@@ -15,234 +15,229 @@ import { Params, useActionData, useFetcher } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import Alert from '@mui/material/Alert';
 /*
-    ? Content-Type 連結紀錄
-    application/json
-    ? submit 可以 submit FormData
-    ? FormData 是甚麼   
-    ! 紀錄 useActionData    
-    <Form method="post"> or useSubmit() 才使用
-    使用 fetcher.data 資料會存在這邊
-    onSubmit 可以覆蓋
-    material UI 
-        TextField 功能
+   ? FormData 是甚麼   
     
-    fetcher.formdata 使用方式
-
+        
     ? 加入 eslint?? dotnet ??
+    ? fetcher.formdata 使用方式
+    ? loader 觸發時機  只有在換頁或是action 呼叫時在會觸發(action 呼叫時在會觸發?
+    <Form method="post" action="/songs" />;  action 為送到哪個路由的 action?
+
+    換頁
 */
 
 export async function action({ params, request }: { params: Params, request: Request }) {
-    let formData: FormData = await request.formData();
+  let formData: FormData = await request.formData();
 
-    const response = await fetch("api/account/login", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ email: formData.get("email"), password: formData.get("password") })
-    });
-    const responseData = await response.json();
+  const response = await fetch("api/account/login", {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ email: formData.get("email"), password: formData.get("password") })
+  });
+  const responseData = await response.json();
 
-    if (!response.ok) {
+  if (!response.ok) {
 
-        return responseData;
-    } else {
+    return responseData;
+  } else {
 
-    }
+  }
 
-    console.log("data", responseData);
-    // 做換頁
+  console.log("data", responseData);
+  // 做換頁
 }
 
 
 
 
 const Card = styled(MuiCard)(({ theme }) => ({
-    display: 'flex',
-    flexDirection: 'column',
-    alignSelf: 'center',
-    width: '100%',
-    padding: theme.spacing(4),
-    gap: theme.spacing(2),
-    margin: 'auto',
-    [theme.breakpoints.up('sm')]: {
-        maxWidth: '450px',
-    },
+  display: 'flex',
+  flexDirection: 'column',
+  alignSelf: 'center',
+  width: '100%',
+  padding: theme.spacing(4),
+  gap: theme.spacing(2),
+  margin: 'auto',
+  [theme.breakpoints.up('sm')]: {
+    maxWidth: '450px',
+  },
+  boxShadow:
+    'hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px',
+  ...theme.applyStyles('dark', {
     boxShadow:
-        'hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px',
-    ...theme.applyStyles('dark', {
-        boxShadow:
-            'hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px',
-    }),
+      'hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px',
+  }),
 }));
 
 const SignInContainer = styled(Stack)(({ theme }) => ({
-    minHeight: '100%',
-    minWidth: "500px",
-    padding: theme.spacing(2),
-    [theme.breakpoints.up('sm')]: {
-        padding: theme.spacing(4),
-    },
-    '&::before': {
-        content: '""',
-        display: 'block',
-        position: 'absolute',
-        zIndex: -1,
-        inset: 0,
-        backgroundImage:
-            'radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))',
-        backgroundRepeat: 'no-repeat',
-        ...theme.applyStyles('dark', {
-            backgroundImage:
-                'radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))',
-        }),
-    },
+  minHeight: '100%',
+  minWidth: "500px",
+  padding: theme.spacing(2),
+  [theme.breakpoints.up('sm')]: {
+    padding: theme.spacing(4),
+  },
+  '&::before': {
+    content: '""',
+    display: 'block',
+    position: 'absolute',
+    zIndex: -1,
+    inset: 0,
+    backgroundImage:
+      'radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))',
+    backgroundRepeat: 'no-repeat',
+    ...theme.applyStyles('dark', {
+      backgroundImage:
+        'radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))',
+    }),
+  },
 }));
 
 export function SignInPage(props: { disableCustomTheme?: boolean }) {
-    const [emailError, setEmailError] = useState(false);
-    const [emailErrorMessage, setEmailErrorMessage] = useState('');
-    const [passwordError, setPasswordError] = useState(false);
-    const [passwordErrorMessage, setPasswordErrorMessage] = useState('');
-    const [open, setOpen] = useState(false);
-    let fetcher = useFetcher();
-    const response = fetcher.data;
+  const [emailError, setEmailError] = useState(false);
+  const [emailErrorMessage, setEmailErrorMessage] = useState('');
+  const [passwordError, setPasswordError] = useState(false);
+  const [passwordErrorMessage, setPasswordErrorMessage] = useState('');
+  const [open, setOpen] = useState(false);
+  let fetcher = useFetcher();
+  const response = fetcher.data;
 
 
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        const submitFormData = new FormData(event.currentTarget);
-        if (validateInputs()) {
-            fetcher.submit(submitFormData, { method: "post", action: "/" })
-        }
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const submitFormData = new FormData(event.currentTarget);
+    if (validateInputs()) {
+      fetcher.submit(submitFormData, { method: "post", action: "/" })
+    }
+  }
+
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+
+
+  const validateInputs = () => {
+    const email = document.getElementById('email') as HTMLInputElement;
+    const password = document.getElementById('password') as HTMLInputElement;
+
+    let isValid = true;
+
+    if (!email.value || !/\S+@\S+\.\S+/.test(email.value)) {
+      setEmailError(true);
+      setEmailErrorMessage('Please enter a valid email address.');
+      isValid = false;
+    } else {
+      setEmailError(false);
+      setEmailErrorMessage('');
     }
 
+    if (!password.value || password.value.length < 6) {
+      setPasswordError(true);
+      setPasswordErrorMessage('Password must be at least 6 characters long.');
+      isValid = false;
+    } else {
+      setPasswordError(false);
+      setPasswordErrorMessage('');
+    }
 
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
+    return isValid;
+  };
 
-    const handleClose = () => {
-        setOpen(false);
-    };
-
-
-
-    const validateInputs = () => {
-        const email = document.getElementById('email') as HTMLInputElement;
-        const password = document.getElementById('password') as HTMLInputElement;
-
-        let isValid = true;
-
-        if (!email.value || !/\S+@\S+\.\S+/.test(email.value)) {
-            setEmailError(true);
-            setEmailErrorMessage('Please enter a valid email address.');
-            isValid = false;
-        } else {
-            setEmailError(false);
-            setEmailErrorMessage('');
-        }
-
-        if (!password.value || password.value.length < 6) {
-            setPasswordError(true);
-            setPasswordErrorMessage('Password must be at least 6 characters long.');
-            isValid = false;
-        } else {
-            setPasswordError(false);
-            setPasswordErrorMessage('');
-        }
-
-        return isValid;
-    };
-
-    return (
-        <>
-            <SignInContainer direction="column" justifyContent="space-between">
-                <Card variant="outlined">
-                    <Typography
-                        component="h1"
-                        variant="h4"
-                        sx={{ width: '100%', fontSize: 'clamp(2rem, 10vw, 2.15rem)' }}
-                    >
-                        Sign in
-                    </Typography>
-                    <fetcher.Form method="post" onSubmit={handleSubmit} >
-                        <Box
-                            sx={{
-                                display: 'flex',
-                                flexDirection: 'column',
-                                width: '100%',
-                                gap: 2,
-                            }}
-                        >
-                            <FormControl>
-                                <Box sx={{ display: 'flex', justifyContent: 'start' }}>
-                                    <FormLabel htmlFor="email">Email</FormLabel>
-                                </Box>
-                                <TextField
-                                    error={emailError}
-                                    helperText={emailErrorMessage}
-                                    id="email"
-                                    type="email"
-                                    name="email"
-                                    placeholder="your@email.com"
-                                    autoComplete="email"
-                                    autoFocus
-                                    required
-                                    fullWidth
-                                    variant="outlined"
-                                    color={emailError ? 'error' : 'primary'}
-                                    sx={{ ariaLabel: 'email' }}
-                                />
-                            </FormControl>
-                            <FormControl>
-                                <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-                                    <FormLabel htmlFor="password">Password</FormLabel>
-                                    <Link
-                                        component="button"
-                                        type="button"
-                                        onClick={handleClickOpen}
-                                        variant="body2"
-                                        sx={{ alignSelf: 'baseline' }}
-                                    >
-                                        Forgot your password?
-                                    </Link>
-                                </Box>
-                                <TextField
-                                    error={passwordError}
-                                    helperText={passwordErrorMessage}
-                                    name="password"
-                                    placeholder="••••••"
-                                    type="password"
-                                    id="password"
-                                    autoComplete="current-password"
-                                    autoFocus
-                                    required
-                                    fullWidth
-                                    variant="outlined"
-                                    color={passwordError ? 'error' : 'primary'}
-                                />
-                            </FormControl>
-                            <FormControlLabel
-                                control={<Checkbox value="remember" color="primary" />}
-                                label="Remember me"
-                            />
-                            {response && <Alert severity="error">
-                                {response.message}
-                            </Alert>}
-                            <ForgotPassword open={open} handleClose={handleClose} />
-                            <Button
-                                type="submit"
-                                fullWidth
-                                variant="contained"
-                                onClick={validateInputs}
-                            >
-                                Sign in
-                            </Button>
-                        </Box>
-                    </fetcher.Form>
-                </Card>
-            </SignInContainer>
-        </>
-    );
+  return (
+    <>
+      <SignInContainer direction="column" justifyContent="space-between">
+        <Card variant="outlined">
+          <Typography
+            component="h1"
+            variant="h4"
+            sx={{ width: '100%', fontSize: 'clamp(2rem, 10vw, 2.15rem)' }}
+          >
+            Sign in
+          </Typography>
+          <fetcher.Form method="post" onSubmit={handleSubmit} >
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                width: '100%',
+                gap: 2,
+              }}
+            >
+              <FormControl>
+                <Box sx={{ display: 'flex', justifyContent: 'start' }}>
+                  <FormLabel htmlFor="email">Email</FormLabel>
+                </Box>
+                <TextField
+                  error={emailError}
+                  helperText={emailErrorMessage}
+                  id="email"
+                  type="email"
+                  name="email"
+                  placeholder="your@email.com"
+                  autoComplete="email"
+                  autoFocus
+                  required
+                  fullWidth
+                  variant="outlined"
+                  color={emailError ? 'error' : 'primary'}
+                  sx={{ ariaLabel: 'email' }}
+                />
+              </FormControl>
+              <FormControl>
+                <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                  <FormLabel htmlFor="password">Password</FormLabel>
+                  <Link
+                    component="button"
+                    type="button"
+                    onClick={handleClickOpen}
+                    variant="body2"
+                    sx={{ alignSelf: 'baseline' }}
+                  >
+                    Forgot your password?
+                  </Link>
+                </Box>
+                <TextField
+                  error={passwordError}
+                  helperText={passwordErrorMessage}
+                  name="password"
+                  placeholder="••••••"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                  autoFocus
+                  required
+                  fullWidth
+                  variant="outlined"
+                  color={passwordError ? 'error' : 'primary'}
+                />
+              </FormControl>
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              {response && <Alert severity="error">
+                {response.message}
+              </Alert>}
+              <ForgotPassword open={open} handleClose={handleClose} />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                onClick={validateInputs}
+              >
+                Sign in
+              </Button>
+            </Box>
+          </fetcher.Form>
+        </Card>
+      </SignInContainer>
+    </>
+  );
 }
