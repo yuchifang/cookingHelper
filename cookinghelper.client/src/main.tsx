737779e4725240078fb4f1c42df3e1c1c26c0362
fangfelipe <fangfelipe@gmail.com>
2024-10-30 18:02:08 +0800

    add router adminPage LoginPage, add GlobalStyles

--- cookinghelper.client/src/main.tsx
+++ cookinghelper.client/src/main.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import SingInPage from "./SingInPage";
+import AdminPage from "./AdminPage";
+import "./index.css";
+import GlobalStyles from "@mui/material/GlobalStyles";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AppTEST from "./AppTest";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+/*
+  login後 進 /admin 的 route
+*/
+const router = createBrowserRouter([SingInPage, AppTEST, AdminPage]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <GlobalStyles
+      styles={{
+        "#root": {
+          maxWidth: "100%",
+          margin: "0 auto",
+          padding: "2rem",
+          TextAlign: "center",
+        },
+      }}
+    />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
