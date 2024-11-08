cd5bc560025a0e5636e760c14f1e05445b257d35
fangfelipe <fangfelipe@gmail.com>
2024-11-08 16:40:01 +0800

    add Extension

---
+++ CookingHelper.Server/Extension/OwnExtension.cs
@@ -0,0 +1,7 @@
+namespace CookingHelper.Extension;
+
+public static class IEnumerableExtensions
+{
+    public static IEnumerable<(T item, int index)> WithIndex<T>(this IEnumerable<T> self) =>
+        self.Select((item, index) => (item, index));
+}
