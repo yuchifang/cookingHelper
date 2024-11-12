a89092ee4945578d7e421443dd3192be097f2924
fangfelipe <fangfelipe@gmail.com>
2024-11-12 11:09:51 +0800

    comment loader

--- cookinghelper.client/src/AdminPage/index.tsx
+++ cookinghelper.client/src/AdminPage/index.tsx
@@ -1,9 +1,10 @@
 import ErrorElement from "../ErrorElement";
-import { AdminPage, loader } from "./AdminPage";
+// import { AdminPage, loader } from "./AdminPage";
+import { AdminPage } from "./AdminPage";
 
 export default {
   path: "/admin",
   element: <AdminPage />,
   ErrorElement: <ErrorElement />,
-  loader: loader,
+  // loader: loader,
 };
