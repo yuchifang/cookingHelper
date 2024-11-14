d8c9c68519857502722f84cf552dec7878e79e9b
fangfelipe <fangfelipe@gmail.com>
2024-11-14 16:43:26 +0800

    add commend

--- CookingHelper.Server/Model/ApiLog.cs
+++ CookingHelper.Server/Model/ApiLog.cs
@@ -2,7 +2,7 @@ public class ApiLog
 {
     public int Id { get; set; } = default!;
 
-    // UTC+0
+    // UTC+0 Second
     public long LogTime { get; set; } = default!;
     public string UserId { get; set; } = default!;
 }
