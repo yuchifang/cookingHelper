87516f647d55387ab9cf5656d76959a71f90a324
fangfelipe <fangfelipe@gmail.com>
2024-11-14 21:17:08 +0800

    remove loader

--- cookinghelper.client/src/AdminPage/index.tsx
+++ cookinghelper.client/src/AdminPage/index.tsx
@@ -1,10 +1,9 @@
 import ErrorElement from "../ErrorElement";
-import { AdminPage, loader } from "./AdminPage";
+import { AdminPage } from "./AdminPage";
 // import { AdminPage } from "./AdminPage";
 
 export default {
   path: "/admin",
   element: <AdminPage />,
   errorElement: <ErrorElement />,
-  loader: loader,
 };
