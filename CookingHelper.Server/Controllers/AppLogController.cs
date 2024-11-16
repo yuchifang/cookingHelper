bd4680370a173dcfc3723843efdac7bd66afced4
fangfelipe <fangfelipe@gmail.com>
2024-11-16 23:42:20 +0800

    add base generate barChart Date, and Special Circumstances

--- CookingHelper.Server/Controllers/AppLogController.cs
+++ CookingHelper.Server/Controllers/AppLogController.cs
@@ -14,12 +14,12 @@ public class AppLogController : ControllerBase
     }
 
     [HttpGet("getLogList")]
-    //! UTC +0 Time
+    //! UTC +0 Second Time
     public async Task<ActionResult> GetLogList(string startTime, string endTime, string dateUnit)
     {
         // dateUnit 日 月 年
         long unit;
-        bool isExceedLimit = false; // 超過 100 筆計算方式會改變
+
         Console.WriteLine(startTime + "startTime");
         var startTimeLong = Convert.ToInt64(startTime);
         var endTimeLong = Convert.ToInt64(endTime);
@@ -38,62 +38,128 @@ public class AppLogController : ControllerBase
 
         var endTimeOffset = DateTimeOffset.FromUnixTimeSeconds(endTimeLong);
         var endDateTime = endTimeOffset.DateTime;
+        //! 簡化這部分
 
-        // 超過 100 筆計算方式會改變
+        //! 完成 year
+        //! 建立 圖表的 page
+        //! 建立 日歷的 page
+        //! 建立 分頁的 page
+
+        //! 加 alert
+        //! 只要 string 前五個字
         if (dateUnit == "day")
         {
+            // 統計 logList的資料,並去除重複的
+            var BarChartList = logList
+                .Select(entry => new
+                { //! 轉 UTC +8
+                    Date = DateTimeOffset.FromUnixTimeSeconds(entry.LogTime).LocalDateTime.Date,
+                    entry.UserId
+                })
+                .Distinct()
+                .GroupBy(entry => entry.Date)
+                .Select(group => new ChatBar
+                {
+                    Date = DateOnly.FromDateTime(group.Key),
+                    Count = group.Count()
+                });
+
             unit = 60 * 60 * 24;
             if ((endTimeLong - startTimeLong) / unit > 100)
             {
-                isExceedLimit = true;
+                var BarChartListList = BarChartList.OrderBy(entry => entry.Date).ToList();
+                var limitCount = 100;
+                var random = new Random();
+                var offset = (endDateTime - startDateTime) / limitCount;
+                Console.WriteLine(offset + "offset");
+                Console.WriteLine(startDateTime + "startDateTime");
+                Console.WriteLine(endDateTime + "endDateTime");
+                // 依時間單位,時間範圍產生 所有的空資料, 有值則填值
+                var DateList = Enumerable
+                    .Range(1, limitCount)
+                    .Select((space) => startDateTime.Add(offset * space))
+                    //! UTC+8
+                    .Select(date => DateOnly.FromDateTime(date.ToLocalTime()))
+                    .Select(date =>
+                    {
+                        var tempCount = 0;
+                        var itemsToRemove = new List<ChatBar>();
+                        foreach (ChatBar barChartItem in BarChartListList)
+                        {
+                            if (barChartItem.Date < date)
+                            {
+                                tempCount += barChartItem.Count;
+                                itemsToRemove.Add(barChartItem);
+                            }
+                        }
+                        foreach (var item in itemsToRemove)
+                        {
+                            BarChartListList.Remove(item);
+                        }
+                        return new { Date = date.ToString("yyyy-MM-dd"), Count = tempCount };
+                    })
+                    .OrderBy(entry => entry.Date)
+                    .ToList();
+
+                return Ok(DateList);
             }
+
+            var BarChartDic = BarChartList.ToDictionary(entry => entry.Date, entry => entry.Count);
+            var BarChartData = Enumerable
+                .Range(0, (endDateTime - startDateTime).Days + 1)
+                .Select(offset => startDateTime.AddDays(offset))
+                //! UTC+8
+                .Select(date => DateOnly.FromDateTime(date.ToLocalTime()))
+                .Select(date => new
+                {
+                    Date = date.ToString("yyyy-MM-dd"),
+                    Count = BarChartDic.ContainsKey(date) ? BarChartDic[date] : 0
+                })
+                .OrderBy(entry => entry.Date)
+                .ToList();
+
+            return Ok(BarChartData);
         }
         else if (dateUnit == "month")
         {
+            var BarChartDic = logList
+                .Select(entry => new
+                { //! 轉 UTC +8
+                    Date = DateTimeOffset
+                        .FromUnixTimeSeconds(entry.LogTime)
+                        .LocalDateTime.ToString("yyyy-MM"), // todo month
+                    entry.UserId
+                })
+                .Distinct()
+                .GroupBy(entry => entry.Date)
+                .Select(group => new
+                {
+                    Date = group.Key, //todo month
+                    Count = group.Count()
+                })
+                .ToDictionary(entry => entry.Date, entry => entry.Count);
             var totalMonth =
                 ((endDateTime.Year - startDateTime.Year) * 12)
                 + (endDateTime.Month - startDateTime.Month);
+
             if (totalMonth > 100)
             {
-                isExceedLimit = true;
-            }
-        }
-        else if (dateUnit == "year")
-        {
-            var totalYear = endDateTime.Year - startDateTime.Year;
-            if (totalYear > 100)
-            {
-                isExceedLimit = true;
-            }
-        }
+                var sampleCount = 100;
+                var random = new Random();
 
-        if (!isExceedLimit)
-        {
-            if (dateUnit == "day")
-            {
-                // 統計 logList的資料,並去除重複的
-                var BarChartDic = logList
-                    .Select(entry => new
-                    { //! 轉 UTC +8
-                        Date = DateTimeOffset.FromUnixTimeSeconds(entry.LogTime).LocalDateTime.Date,
-                        entry.UserId
-                    })
-                    .Distinct()
-                    .GroupBy(entry => entry.Date)
-                    .Select(group => new
-                    {
-                        Date = DateOnly.FromDateTime(group.Key).ToString("yyyy-MM-dd"),
-                        Count = group.Count()
-                    })
-                    .ToDictionary(entry => entry.Date, entry => entry.Count);
-                // 依時間單位,時間範圍產生 所有的空資料, 有值則填值
-                var BarChartData = Enumerable
-                    .Range(0, (endDateTime - startDateTime).Days + 1)
-                    .Select(offset => startDateTime.AddDays(offset))
-                    //! UTC+8
-                    .Select(date =>
-                        DateOnly.FromDateTime(date.ToLocalTime()).ToString("yyyy-MM-dd")
+                var DateList = Enumerable
+                    .Range(
+                        0,
+                        endDateTime.Month
+                            - startDateTime.Month
+                            + 12 * (endDateTime.Year - startDateTime.Year)
+                            + 1
                     )
+                    .Select(startDateTime.AddMonths)
+                    //! UTC+8
+                    .Select(date => DateOnly.FromDateTime(date.ToLocalTime()).ToString("yyyy-MM"))
+                    .OrderBy(x => random.Next())
+                    .Take(sampleCount)
                     .Select(date => new
                     {
                         Date = date,
@@ -101,62 +167,85 @@ public class AppLogController : ControllerBase
                     })
                     .OrderBy(entry => entry.Date)
                     .ToList();
-
-                return Ok(BarChartData);
+                return Ok(DateList);
             }
+            //! Timespan Page
+            var BarChartData = Enumerable
+                .Range(
+                    0,
+                    endDateTime.Month
+                        - startDateTime.Month
+                        + 12 * (endDateTime.Year - startDateTime.Year)
+                        + 1
+                ) //todo month
+                .Select(startDateTime.AddMonths)
+                //! UTC+8
+                .Select(date => DateOnly.FromDateTime(date.ToLocalTime()).ToString("yyyy-MM"))
+                .Select(date => new
+                {
+                    Date = date,
+                    Count = BarChartDic.ContainsKey(date) ? BarChartDic[date] : 0
+                })
+                .OrderBy(entry => entry.Date)
+                .ToList();
+            return Ok(BarChartData);
         }
-        else { } // 建立超過 limit 沒超過 limit 的算法
-        return Ok();
+        else if (dateUnit == "year")
+        {
+            var BarChartDic = logList
+                .Select(entry => new
+                { //! 轉 UTC +8
+                    Date = DateTimeOffset
+                        .FromUnixTimeSeconds(entry.LogTime)
+                        .LocalDateTime.ToString("yyyy"), // todo month
+                    entry.UserId
+                })
+                .Distinct()
+                .GroupBy(entry => entry.Date)
+                .Select(group => new { Date = group.Key, Count = group.Count() })
+                .ToDictionary(entry => entry.Date, entry => entry.Count);
+            var totalYear = endDateTime.Year - startDateTime.Year;
+            if (totalYear > 100)
+            {
+                var sampleCount = 100;
+                var random = new Random();
+
+                var DateList = Enumerable
+                    .Range(0, endDateTime.Year - startDateTime.Year + 1)
+                    .Select(startDateTime.AddYears)
+                    //! UTC+8
+                    .Select(date => DateOnly.FromDateTime(date.ToLocalTime()).ToString("yyyy"))
+                    .OrderBy(x => random.Next())
+                    .Take(sampleCount)
+                    .Select(date => new
+                    {
+                        Date = date,
+                        Count = BarChartDic.ContainsKey(date) ? BarChartDic[date] : 0
+                    })
+                    .OrderBy(entry => entry.Date)
+                    .ToList();
+                return Ok(DateList);
+            }
+            var BarChartData = Enumerable
+                .Range(0, endDateTime.Year - startDateTime.Year + 1)
+                .Select(startDateTime.AddYears)
+                //! UTC+8
+                .Select(date => DateOnly.FromDateTime(date.ToLocalTime()).ToString("yyyy"))
+                .Select(date => new
+                {
+                    Date = date,
+                    Count = BarChartDic.ContainsKey(date) ? BarChartDic[date] : 0
+                })
+                .OrderBy(entry => entry.Date)
+                .ToList();
+            return Ok(BarChartData);
+        }
+        return NotFound();
+    }
+
+    public class ChatBar
+    {
+        public DateOnly Date { get; set; } = default!;
+        public int Count { get; set; } = default!;
     }
 }
-
-/*
- 
-        var students = new List<Student>
-        {
-            new Student
-            {
-                Name = "Alice",
-                Grade = "A",
-                Score = 85
-            },
-            new Student
-            {
-                Name = "Bob",
-                Grade = "B",
-                Score = 75
-            },
-            new Student
-            {
-                Name = "Charlie",
-                Grade = "A",
-                Score = 95
-            },
-            new Student
-            {
-                Name = "David",
-                Grade = "B",
-                Score = 80
-            },
-            new Student
-            {
-                Name = "Eve",
-                Grade = "C",
-                Score = 70
-            }
-        };
-
-        // 使用 GroupBy 按年級分組
-        var groupedStudents = students.GroupBy(student => student.Grade);
-
-        // 輸出分組結果和平均分數
-        foreach (var group in groupedStudents)
-        {
-            Console.WriteLine($"年級: {group.Key}, 平均分數: {group.Count()}");
-        }
-        public class Student
-    {
-        public string Name { get; set; }
-        public string Grade { get; set; }
-        public int Score { get; set; }
-    }*/
