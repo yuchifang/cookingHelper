9a5158b01ed3470e37008efbd6f585bda85adad6
fangfelipe <fangfelipe@gmail.com>
2024-11-24 13:13:35 +0800

    remove userName

--- CookingHelper.Server/Controllers/AccountIdentityController.cs
+++ CookingHelper.Server/Controllers/AccountIdentityController.cs
@@ -108,7 +108,6 @@ public class AccountIdentityController : ControllerBase
 
 public class RegisterModel
 {
-    public string Username { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
     public string Permission { get; set; }
