23c6c709961d189c761d605e89200b03433957e2
fangfelipe <fangfelipe@gmail.com>
2024-12-04 11:07:24 +0800

    remove comment

--- CookingHelper.Server/Service/EmailSender.cs
+++ CookingHelper.Server/Service/EmailSender.cs
@@ -8,9 +8,6 @@ public class EmailSender : IEmailSender
 {
     public Task SendEmailAsync(string email, string subject, string htmlMessage)
     {
-        // 使用 SMTP、SendGrid 或其他服務發送電子郵件
-        // SendGrid
-        // UseDefaultCredentials 功用 建立 page
         using (var client = new SmtpClient())
         {
             client.Host = "smtp.gmail.com";
@@ -35,8 +32,7 @@ public class EmailSender : IEmailSender
                 client.Send(message);
             }
         }
-        // todo SMTP
-        // todo 完成 這個
+
         return Task.CompletedTask;
     }
 }
