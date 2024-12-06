59680d8a78659385bfc00623dda433418a850f05
fangfelipe <fangfelipe@gmail.com>
2024-12-06 16:47:55 +0800

    fix non-use code

--- cookinghelper.client/src/AdminPage/AdminPage.tsx
+++ cookinghelper.client/src/AdminPage/AdminPage.tsx
@@ -57,7 +57,7 @@ export function AdminPage() {
     tabList.push(<Tab label="帳號管理" sx={{ fontSize: "25px" }} />);
   }
 
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+  const handleChange = (_event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
   };
 
